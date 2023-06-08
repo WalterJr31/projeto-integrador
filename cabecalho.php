777282fb8b1897d028dcf8a822bd97bd991963bb
Walter Junior <walter.asjunior31@gmail.com>
2023-06-07 22:11:59 -0300

    continuação

---
+++ cabecalho.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="Background.css">
+</head>
+<body class="">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark  border-bottom shadow-sm" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href=""><img src="https://images-americanas.b2w.io/spacey/acom/2023/02/15/iphone-3787be21d6a7.png" alt="logo" class="alteracao pe-1">MobileMasters</a>
+            <div>
+                <ul class="navbar-nav flex-grow-1">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="index-login.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="produtos.php">Produtos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <ul class="navbar-nav flex-grow-1">
+                <li class="nav-item">
+                    <a class="nav-link btn text-warning carrinho border border-warning me-2" href="carrinho.php">CARRINHO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn text-danger border border-danger" href="index.php">SAIR</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
